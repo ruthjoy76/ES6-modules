43d8b7f2a31297f8e46764d304839262dab844a7
ruthjoy76 <ruthjoy76@gmail.com>
2023-04-20 21:02:23 +0800

    first commit

---
+++ vite-project/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+import { add, subtract, multiply, divide } from "./src/math";
+
+console.log(add(3, 4));
+console.log(subtract(6, 8));
+console.log(multiply(9, 12));
+console.log(divide(12, 16));
---
+++ vite-project/src/calculate.js
@@ -0,0 +1,16 @@
+import { add, divide, multiply, subtract } from "./math";
+
+export default function calculate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return null;
+  }
+}
